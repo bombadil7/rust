cfacec9fa71416e4b0ac4d59b9a214ea653ea27f
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2019-09-13 21:27:39 -0700

    Add search function and test for minigrep
    Minigrep now extracts all lines containing given string
    and prints them to the screen.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -19,20 +19,51 @@ impl Config {
     }
 }
 
-
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn accepts_searchstring_param() {}
+    fn config_new_works() {
+        let vec = vec![
+            String::from("abc"),
+            String::from("def"),
+            String::from("ghi"),
+        ];
+        let conf = Config::new(&vec).unwrap();
+        assert_eq!(conf.filename, "ghi");
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
