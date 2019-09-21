68f0901992011984eec9f9049c34f85c122a4c84
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2019-09-21 13:11:24 -0700

    Fix fizzbuzz
    Changed return value from &str to String.
    This way numbers work as expected and I
    can use simple val.to_string() to convert.
    
    Also with strings appending buzz works.

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,21 +1,23 @@
 fn main() {
-    for i in 1..16 {
+    for i in 1..101 {
         println!("{}", fizzbuzz(i));
     }
 }
 
-fn fizzbuzz<'a>(val: u32) -> &'a str {
-    if val % 3 == 0 && val % 5 == 0 {
-        "fizzbuzz"
-    } else if val % 3 == 0 {
-        "fizz"
-    } else if val % 5 == 0 {
-        "buzz"
-    } else {
-        let s: String = val.to_string();
-        //let ss = &s[..];
-        println!("{}", s);
-        //ss.clone()
-        ""
+fn fizzbuzz(val: u32) -> String {
+    let mut str: String = String::new();
+
+    if val % 3 == 0 {
+        str = str + &String::from("fizz");
     }
+
+    if val % 5 == 0 {
+        str = str + &String::from("buzz");
+    }
+
+    if str.len() < 1 {
+        str = val.to_string();
+    }
+
+    str
 }
