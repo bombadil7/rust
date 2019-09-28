fc0109c99e9cf73726f1ffb16387fe0715c4903a
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2019-09-27 17:57:36 -0700

    Convert nested matchesinto consecutive let var = match constructs

--- args2parse2/src/main.rs
+++ args2parse2/src/main.rs
@@ -16,33 +16,35 @@ fn parse_args() -> Result<Frame, ParseError> {
 
     let mut args = std::env::args().skip(1);
 
+    let width_str = match args.next() {
+            None => return Err(TooFewArgs),
+            Some(width_str) => width_str,
+    };
+
+    let height_str = match args.next() {
+            None => return Err(TooFewArgs),
+            Some(height_str) => height_str, 
+    };
+
     match args.next() {
-        None => Err(TooFewArgs),
-        Some(width_str) => {
-            match args.next() {
-                None => Err(TooFewArgs),
-                Some(height_str) => {
-                    match args.next() {
-                        Some(_) => Err(TooManyArgs),
-                        None => {
-                            match width_str.parse() {
-                                Err(_) => Err(InvalidInteger(width_str)),
-                                Ok(width) => {
-                                    match height_str.parse() {
-                                        Err(_) => Err(InvalidInteger(height_str)),
-                                        Ok(height) => Ok(Frame {
-                                            width,
-                                            height,
-                                        }),
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+        Some(_) => return Err(TooManyArgs),
+        None => (),
+    } 
+
+    let width = match width_str.parse() {
+        Err(_) => return Err(InvalidInteger(width_str)),
+        Ok(width) => width,
+    };
+
+    let height = match height_str.parse() {
+        Err(_) => return Err(InvalidInteger(height_str)),
+        Ok(height) => height,
+    };
+
+    Ok(Frame {
+        width,
+        height,
+    })
 }
 
 fn main() {
