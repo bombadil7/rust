904c84d7db9cb2a4bf17e1f041aa110a40fcefba
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2019-10-05 08:56:21 -0700

    Fix OneToTen iterator

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -22,7 +22,8 @@ struct OneToTen(u32);
 
 impl OneToTen {
     fn new() -> OneToTen {
-        OneToTen {0: 0 }
+        //OneToTen {0: 0 }
+        OneToTen(0)
     }
 }
 
@@ -30,15 +31,15 @@ impl Iterator for OneToTen {
     type Item = u32;
 
     fn next(&mut self) -> Option<u32> {
-        OneToTen.0 += 1;
-        Some( OneToTen.0 )
+        self.0 += 1;
+        Some(self.0)
     }
 }
 
 fn main() {
     let num = OneToTen::new();
 
-    for i in OneToTen.take(10) {
+    for i in num.take(10) {
         println!("The answer to life, the universe, and everything is {}", i);
     }
     println!("All done!");
