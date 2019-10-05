f26be7dea503b10a5fedb2bae478fc229590dcb0
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2019-10-05 08:43:06 -0700

    First attempt at one_to_ten iterator

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -8,18 +8,37 @@
 //    }
 //}
 
-struct TheAnswer;
+//struct TheAnswer;
+//
+//impl Iterator for TheAnswer {
+//    type Item = u32;
+//
+//    fn next(&mut self) -> Option<u32> {
+//        Some(42)
+//    }
+//}
+//
+struct OneToTen(u32);
 
-impl Iterator for TheAnswer {
+impl OneToTen {
+    fn new() -> OneToTen {
+        OneToTen {0: 0 }
+    }
+}
+
+impl Iterator for OneToTen {
     type Item = u32;
 
     fn next(&mut self) -> Option<u32> {
-        Some(42)
+        OneToTen.0 += 1;
+        Some( OneToTen.0 )
     }
 }
 
 fn main() {
-    for i in TheAnswer.take(10) {
+    let num = OneToTen::new();
+
+    for i in OneToTen.take(10) {
         println!("The answer to life, the universe, and everything is {}", i);
     }
     println!("All done!");
