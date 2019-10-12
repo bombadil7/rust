3888b4a68edfb57c816f61130072c2581dafb4ea
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2019-10-12 16:14:23 -0700

    Add testdoc section to the library

--- syntactic-macro/src/lib.rs
+++ syntactic-macro/src/lib.rs
@@ -1,3 +1,19 @@
+// Network Programming with Rust
+// Chapter 2 factorial lib.rs
+
+/// The factorial crate provides a macro to compute factorial of a given 
+/// integer
+/// # Examples
+/// 
+/// ```
+/// # #[macro_use] extern crate syntactic_macro;
+/// # fn main() {
+/// assert_eq!(factorial!(0), 1);
+/// assert_eq!(factorial!(6), 720);
+/// # }
+/// ```
+/// 
+
 #[allow(unused_macros)]
 #[macro_export]
 macro_rules! factorial {
@@ -19,5 +35,10 @@ mod tests {
     #[test]
     fn test_factorial() {
         assert_eq!(factorial!(5), 120)
+    }
+
+    #[test]
+    fn test_factorial_fail() {
+        assert_eq!(factorial!(5), 121);
     }
 }
