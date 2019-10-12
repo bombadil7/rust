8cae8e11ab96ee05f9961fbdf701edd13f83ca4b
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2019-10-12 14:57:03 -0700

    Add threads examples
    threads project shows basic spawn / join
    threads-channels shows example of mpsc for channels

---
+++ threads/src/main.rs
@@ -0,0 +1,13 @@
+// Network Programming with Rust
+// Chapter 2 - threads
+
+use std::thread;
+
+fn main() {
+    for i in 1..10 {
+        let handle = thread::spawn(move || {
+            println!("Hello from thread number {}", i);
+        });
+        let _ = handle.join();
+    }
+}
