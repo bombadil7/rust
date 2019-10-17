13caf3837f890dd2f923ad3614f7b4130dd502de
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2019-10-16 19:45:56 -0700

    Add azul app
    Also some changes to the snake app and new ipnetwork
    example.

--- lifetime2/src/main.rs
+++ lifetime2/src/main.rs
@@ -7,4 +7,7 @@ fn main() {
     let vec2 = vec![1, 2];
 
     println!("{:?}", longer_vector(&vec1, &vec2));
+
+    let vec: Vec<std::String> = "127.0.0.1:8.8.8.8".parse().unwrap();
+    println!("{} {}", a, b);
 }
--- snake/src/draw.rs
+++ snake/src/draw.rs
@@ -3,7 +3,7 @@ use piston_window::types::Color;
 
 const BLOCK_SIZE: f64 = 25.0;
 
-pub fn to_coord(game_coord: i32) -> f64 {
+fn to_coord(game_coord: i32) -> f64 {
     (game_coord as f64) * BLOCK_SIZE
 }
 
